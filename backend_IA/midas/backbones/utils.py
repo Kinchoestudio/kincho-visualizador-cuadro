106ca5169a9a3dd1ff9e98a2213ef2e3c34e870d
Kinchoestudio <kinchoestudio@gmail.com>
2025-05-12 16:52:28 +0200

    Update utils.py

--- backend_IA/midas/backbones/utils.py
+++ backend_IA/midas/backbones/utils.py
@@ -1,34 +1,64 @@
 import torch
-
 import torch.nn as nn
 import numpy as np
+from functools import partial
 
 
-def forward_swin(pretrained, x):
-    return forward_default(pretrained, x)
+# Función de activación sencilla
+def get_activation(name):
+    if name == "relu":
+        return nn.ReLU(inplace=True)
+    elif name == "relu6":
+        return nn.ReLU6(inplace=True)
+    elif name == "leaky_relu":
+        return nn.LeakyReLU(inplace=True)
+    elif name == "selu":
+        return nn.SELU(inplace=True)
+    elif name == "gelu":
+        return nn.GELU()
+    else:
+        raise ValueError(f"Unsupported activation function: {name}")
 
 
-def _make_swin_backbone(
-        model,
-        hooks=[1, 1, 17, 1],
-        patch_grid=[96, 96]
-):
+# Diccionario de activaciones simuladas
+activations = {}
+
+# Forward por defecto
+def forward_default(model, x):
+    return model(x)
+
+# Adaptación para modelos como Swin
+def forward_adapted_unflatten(model, x, method_name="forward_features"):
+    x = getattr(model, method_name)(x)
+    return x.view(x.size(0), -1)
+
+# Método dummy por ahora
+def make_backbone_default(model, *args, **kwargs):
+    return model
+
+# Clase para transponer tensores (usada en act_postprocessX)
+class Transpose(nn.Module):
+    def __init__(self, dim1, dim2):
+        super().__init__()
+        self.dim1 = dim1
+        self.dim2 = dim2
+
+    def forward(self, x):
+        return x.transpose(self.dim1, self.dim2)
+
+
+def _make_swin_backbone(model, hooks=[1, 1, 17, 1], patch_grid=[96, 96]):
     pretrained = nn.Module()
 
     pretrained.model = model
-    pretrained.model.layers[0].blocks[hooks[0]].register_forward_hook(get_activation("1"))
-    pretrained.model.layers[1].blocks[hooks[1]].register_forward_hook(get_activation("2"))
-    pretrained.model.layers[2].blocks[hooks[2]].register_forward_hook(get_activation("3"))
-    pretrained.model.layers[3].blocks[hooks[3]].register_forward_hook(get_activation("4"))
+    pretrained.model.layers[0].blocks[hooks[0]].register_forward_hook(get_activation("relu"))
+    pretrained.model.layers[1].blocks[hooks[1]].register_forward_hook(get_activation("relu"))
+    pretrained.model.layers[2].blocks[hooks[2]].register_forward_hook(get_activation("relu"))
+    pretrained.model.layers[3].blocks[hooks[3]].register_forward_hook(get_activation("relu"))
 
     pretrained.activations = activations
 
-    if hasattr(model, "patch_grid"):
-        used_patch_grid = model.patch_grid
-    else:
-        used_patch_grid = patch_grid
-
-    patch_grid_size = np.array(used_patch_grid, dtype=int)
+    patch_grid_size = np.array(patch_grid, dtype=int)
 
     pretrained.act_postprocess1 = nn.Sequential(
         Transpose(1, 2),
