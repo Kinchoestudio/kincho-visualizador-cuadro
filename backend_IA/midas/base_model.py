ccb67a459882c4865d924ed00d8b5ca5d7933d30
Kinchoestudio <kinchoestudio@gmail.com>
2025-05-12 16:12:56 +0200

    Update base_model.py

--- backend_IA/midas/base_model.py
+++ backend_IA/midas/base_model.py
@@ -1,16 +1,12 @@
 import torch
 
 
-class BaseModel(torch.nn.Module)
-    def load(self, path)
-        Load model from file.
-
-        Args
-            path (str) file path
-        
+class BaseModel(torch.nn.Module):
+    def load(self, path):
+        # Load model from file.
         parameters = torch.load(path, map_location=torch.device('cpu'))
 
-        if optimizer in parameters
-            parameters = parameters[model]
+        if 'optimizer' in parameters:
+            parameters = parameters['model']
 
         self.load_state_dict(parameters)
